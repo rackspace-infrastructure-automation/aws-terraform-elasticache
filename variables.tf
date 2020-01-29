3610d99a01d91cce39398f3058683110374149ec
John Titus <john.titus@rackspace.com>
2020-01-29 17:54:42 -0600

    Sort variables in alphabetic order

--- variables.tf
+++ variables.tf
@@ -1,15 +1,19 @@
-###<Common Parameters>###
-
 variable "additional_tags" {
   description = "(memcached, redis, redis multi shard) Additional tags to be added to the Elasticache resources. Please see examples directory in this repo for examples."
   type        = map(string)
   default     = {}
 }
 
+variable "at_rest_encrypted_disk" {
+  description = "(redis, redis multi shard) Indicates whether to enable encryption at rest. ONLY AVAILABLE FOR REDIS 3.2.6, 4.0.10 AND 5.0.0. `true` or `false`."
+  type        = bool
+  default     = false
+}
+
 variable "cache_cluster_port" {
   description = "(memcached, redis, redis multi shard) The port number on which each of the cache nodes will accept connections. Default for redis is 6379. Default for memcached is 11211"
-  default     = ""
   type        = string
+  default     = ""
 }
 
 variable "cluster_name" {
@@ -19,14 +23,38 @@ variable "cluster_name" {
 
 variable "cluster_name_version" {
   description = "(memcached, redis, redis multi shard) NOTE: This needs to increment on update with new snapshot. If non empty cluster_name_version is being used, total length of cluster_name plus cluster_name_version should not exceed 19 due to string length constraints"
-  default     = "v00"
   type        = string
+  default     = "v00"
+}
+
+variable "cpu_high_evaluations" {
+  description = "(memcached, redis) The number of minutes CPU usage must remain above the specified threshold to generate an alarm."
+  type        = number
+  default     = 5
+}
+
+variable "cpu_high_threshold" {
+  description = "(memcached, redis) The max CPU Usage % before generating an alarm."
+  type        = number
+  default     = 90
 }
 
 variable "create_route53_record" {
   description = "(memcached, redis, redis multi shard) Specifies whether or not to create a route53 CNAME record for the configuration/primary endpoint. internal_zone_id, internal_zone_name, and internal_record_name must be provided if set to true. true or false."
-  default     = false
   type        = bool
+  default     = false
+}
+
+variable "curr_connections_evaluations" {
+  description = "(memcached, redis) The number of minutes current connections must remain above the specified threshold to generate an alarm."
+  type        = number
+  default     = 5
+}
+
+variable "curr_connections_threshold" {
+  description = "(memcached, redis) The max number of current connections before generating an alarm. NOTE: If this variable is not set, the connections alarm will not be provisioned."
+  type        = string
+  default     = ""
 }
 
 variable "elasticache_engine_type" {
@@ -36,8 +64,32 @@ variable "elasticache_engine_type" {
 
 variable "environment" {
   description = "(memcached, redis, redis multi shard) Application environment for which this network is being created. Preferred value are Development, Integration, PreProduction, Production, QA, Staging, or Test"
-  default     = "Development"
   type        = string
+  default     = "Development"
+}
+
+variable "evictions_evaluations" {
+  description = "(memcached, redis) The number of minutes Evictions must remain above the specified threshold to generate an alarm."
+  type        = number
+  default     = 5
+}
+
+variable "evictions_threshold" {
+  description = "(memcached, redis) The max evictions before generating an alarm. NOTE: If this variable is not set, the evictions alarm will not be provisioned."
+  type        = string
+  default     = ""
+}
+
+variable "failover_enabled" {
+  description = "(redis) Enable Multi-AZ Failover. Failover is unsupported on the cache.t1.micro instance class. This is hardcoded as true for Redis multi-shard."
+  type        = bool
+  default     = true
+}
+
+variable "in_transit_encryption" {
+  description = "(redis, redis multi shard) Indicates whether to enable encryption in transit. Because there is some processing needed to encrypt and decrypt the data at the endpoints, enabling in-transit encryption can have some performance impact.ONLY AVAILABLE FOR REDIS 3.2.6 AND 4.0.10. true or false"
+  type        = bool
+  default     = false
 }
 
 variable "instance_class" {
@@ -47,38 +99,62 @@ variable "instance_class" {
 
 variable "internal_record_name" {
   description = "(memcached, redis, redis multi shard) Record Name for the new Resource Record in the Internal Hosted Zone"
-  default     = ""
   type        = string
+  default     = ""
 }
 
 variable "internal_zone_id" {
   description = "(memcached, redis, redis multi shard) The Route53 Internal Hosted Zone ID"
-  default     = ""
   type        = string
+  default     = ""
 }
 
 variable "internal_zone_name" {
   description = "(memcached, redis, redis multi shard) LD for Internal Hosted Zone"
-  default     = ""
   type        = string
+  default     = ""
 }
 
 variable "notification_topic" {
   description = "(memcached, redis, redis multi shard) SNS Topic ARN to notify if there are any alarms"
-  default     = ""
   type        = string
+  default     = ""
+}
+
+variable "number_of_nodes" {
+  description = "(memcached, redis) The number of cache nodes within the ElastiCache cluster. This number must be grearter or equal 2 to enable automatic failover."
+  type        = number
+  default     = 1
+}
+
+variable "number_of_read_replicas_per_shard" {
+  description = "(redis multi shard) Number of read replicas per shard"
+  type        = number
+  default     = 2
+}
+
+variable "number_of_shards" {
+  description = "(redis multi shard) Number of shards"
+  type        = number
+  default     = 2
 }
 
 variable "preferred_maintenance_window" {
   description = "(memcached, redis, redis multi shard) The weekly time range (in UTC) during which system maintenance can occur. Example: Sun:05:00-Sun:07:00"
-  default     = "Sun:05:00-Sun:07:00"
   type        = string
+  default     = "Sun:05:00-Sun:07:00"
 }
 
 variable "redis_multi_shard" {
   description = "(edis, redis multi shard) Is this a redis multi-shard instance? true or false"
-  default     = false
   type        = bool
+  default     = false
+}
+
+variable "replication_group_description" {
+  description = "(redis, redis multi shard) Description of Replication Group"
+  type        = string
+  default     = "Elasticache"
 }
 
 variable "security_group_list" {
@@ -86,142 +162,43 @@ variable "security_group_list" {
   type        = list(string)
 }
 
+variable "snapshot_arn" {
+  description = "(redis, redis multi shard) The S3 ARN of a snapshot to use for cluster creation.  Proper access to the S3 file must be granted prior to building instance.  See https://docs.aws.amazon.com/AmazonElastiCache/latest/UserGuide/backups-seeding-redis.html#backups-seeding-redis-grant-access for details.  This parameter is ignored if a SnapshotName is provided."
+  type        = string
+  default     = ""
+}
+
+variable "snapshot_name" {
+  description = "(redis, redis multi shard) The name of a snapshot to use for cluster creation. This property overrides any value assigned to SnapshotArn. Snapshots are unsupported on cache.t1.micro instance class."
+  type        = string
+  default     = ""
+}
+
+variable "snapshot_retention_limit" {
+  description = "(redis, redis multi shard) The number of days for which automated backups are retained. Setting this parameter to a positive number enables backups. Setting this parameter to 0 disables automated backups. Snapshots are unsupported on cache.t1.micro instance class."
+  type        = number
+  default     = 7
+}
+
+variable "snapshot_window" {
+  description = "(redis, redis multi shard) The daily time range (in UTC) during which ElastiCache will begin taking a daily snapshot of your node group. Snapshots are unsupported on cache.t1.micro instance class."
+  type        = string
+  default     = "03:00-05:00"
+}
+
 variable "subnets" {
   description = "(memcached, redis, redis multi shard) List of subnets for use with this cache cluster"
   type        = list(string)
 }
 
-###<\Common Parameters>###
-
-###<Memcached and Redis non-multi-shard Parameters>###
-
-variable "cpu_high_threshold" {
-  description = "(memcached, redis) The max CPU Usage % before generating an alarm."
-  default     = 90
-  type        = number
-}
-
-variable "cpu_high_evaluations" {
-  description = "(memcached, redis) The number of minutes CPU usage must remain above the specified threshold to generate an alarm."
-  default     = 5
-  type        = number
-}
-
-variable "curr_connections_evaluations" {
-  description = "(memcached, redis) The number of minutes current connections must remain above the specified threshold to generate an alarm."
-  default     = 5
-  type        = number
-}
-
-variable "curr_connections_threshold" {
-  description = "(memcached, redis) The max number of current connections before generating an alarm. NOTE: If this variable is not set, the connections alarm will not be provisioned."
-  default     = ""
-  type        = string
-}
-
-variable "evictions_evaluations" {
-  description = "(memcached, redis) The number of minutes Evictions must remain above the specified threshold to generate an alarm."
-  default     = 5
-  type        = number
-}
-
-variable "evictions_threshold" {
-  description = "(memcached, redis) The max evictions before generating an alarm. NOTE: If this variable is not set, the evictions alarm will not be provisioned."
-  default     = ""
-  type        = string
-}
-
-variable "number_of_nodes" {
-  description = "(memcached, redis) The number of cache nodes within the ElastiCache cluster. This number must be grearter or equal 2 to enable automatic failover."
-  default     = 1
-  type        = number
-}
-
-###<\Memcached and Redis non-multi-shard Parameters>###
-
-###<Redis and Redis multi-shard Parameters>###
-
-variable "in_transit_encryption" {
-  description = "(redis, redis multi shard) Indicates whether to enable encryption in transit. Because there is some processing needed to encrypt and decrypt the data at the endpoints, enabling in-transit encryption can have some performance impact.ONLY AVAILABLE FOR REDIS 3.2.6 AND 4.0.10. true or false"
-  default     = false
-  type        = bool
-}
-
-variable "replication_group_description" {
-  description = "(redis, redis multi shard) Description of Replication Group"
-  default     = "Elasticache"
-  type        = string
-}
-
-variable "snapshot_arn" {
-  description = "(redis, redis multi shard) The S3 ARN of a snapshot to use for cluster creation.  Proper access to the S3 file must be granted prior to building instance.  See https://docs.aws.amazon.com/AmazonElastiCache/latest/UserGuide/backups-seeding-redis.html#backups-seeding-redis-grant-access for details.  This parameter is ignored if a SnapshotName is provided."
-  default     = ""
-  type        = string
-}
-
-variable "snapshot_name" {
-  description = "(redis, redis multi shard) The name of a snapshot to use for cluster creation. This property overrides any value assigned to SnapshotArn. Snapshots are unsupported on cache.t1.micro instance class."
-  default     = ""
-  type        = string
-}
-
-variable "snapshot_window" {
-  description = "(redis, redis multi shard) The daily time range (in UTC) during which ElastiCache will begin taking a daily snapshot of your node group. Snapshots are unsupported on cache.t1.micro instance class."
-  default     = "03:00-05:00"
-  type        = string
-}
-
-variable "at_rest_encrypted_disk" {
-  description = "(redis, redis multi shard) Indicates whether to enable encryption at rest. ONLY AVAILABLE FOR REDIS 3.2.6, 4.0.10 AND 5.0.0. `true` or `false`."
-  default     = false
-  type        = bool
-}
-
-variable "snapshot_retention_limit" {
-  description = "(redis, redis multi shard) The number of days for which automated backups are retained. Setting this parameter to a positive number enables backups. Setting this parameter to 0 disables automated backups. Snapshots are unsupported on cache.t1.micro instance class."
-  default     = 7
-  type        = number
-}
-
-###<\Redis and Redis multi-shard Parameters>###
-
-###<Redis non multi shard Parameters>###
-variable "failover_enabled" {
-  description = "(redis) Enable Multi-AZ Failover. Failover is unsupported on the cache.t1.micro instance class. This is hardcoded as true for Redis multi-shard."
-  default     = true
-  type        = bool
-}
-
-###<\Redis non multi shard Parameters>###
-
-###<Redis multi shard Parameters>###
-
-variable "number_of_read_replicas_per_shard" {
-  description = "(redis multi shard) Number of read replicas per shard"
-  default     = 2
-  type        = number
-}
-
-variable "number_of_shards" {
-  description = "(redis multi shard) Number of shards"
-  default     = 2
-  type        = number
-}
-
-###<\Redis multi shard Parameters>###
-
-###<Memcached Parameters>###
-
 variable "swap_usage_evaluations" {
   description = "(memcached) The number of minutes SwapUsage must remain above the specified threshold to generate an alarm"
-  default     = 5
   type        = number
+  default     = 5
 }
 
 variable "swap_usage_threshold" {
   description = "(memcached) The max SwapUsage before generating an alarm"
-  default     = 52428800
   type        = number
+  default     = 52428800
 }
-
-###<\Memcached Parameters>###
