c7845a5cc42daa8fea79c7f9c48099b38e68d566
John Titus <john.titus@rackspace.com>
2020-01-30 11:15:45 -0600

    Update pinning in tests and examples

--- tests/test1/main.tf
+++ tests/test1/main.tf
@@ -10,57 +10,60 @@ provider "aws" {
 resource "random_string" "r_string" {
   length  = 8
   lower   = true
-  upper   = false
   number  = false
   special = false
+  upper   = false
 }
 
 module "vpc" {
-  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-vpc_basenetwork?ref=v0.0.9"
+  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-vpc_basenetwork?ref=master"
 
-  vpc_name = "ElastiCache-Test-VPC-1"
+  cidr_range          = "172.18.0.0/16"
+  name                = "ElastiCache-Test-VPC-1"
+  private_cidr_ranges = ["172.18.0.0/21", "172.18.8.0/21"]
+  public_cidr_ranges  = ["172.18.168.0/22", "172.18.172.0/22"]
 }
 
 resource "random_string" "zone_name" {
   length  = 10
   lower   = true
-  upper   = false
   number  = false
   special = false
+  upper   = false
 }
 
 module "internal_zone" {
-  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-route53_internal_zone?ref=v0.0.3"
+  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-route53_internal_zone?ref=master"
 
-  zone_name     = "${random_string.zone_name.result}.com"
-  environment   = "Development"
-  target_vpc_id = module.vpc.vpc_id
+  environment = "Development"
+  name        = "${random_string.zone_name.result}.com"
+  vpc_id      = module.vpc.vpc_id
 }
 
 module "security_groups" {
-  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-security_group?ref=v0.0.5"
+  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-security_group?ref=master"
 
-  resource_name = "ElastiCacheTestSG"
-  vpc_id        = module.vpc.vpc_id
-  environment   = "Development"
+  environment = "Development"
+  name        = "ElastiCacheTestSG"
+  vpc_id      = module.vpc.vpc_id
 }
 
 module "elasticache_memcached" {
   source = "../../module"
 
-  cluster_name               = "memc-${random_string.r_string.result}"
-  elasticache_engine_type    = "memcached14"
-  instance_class             = "cache.m4.large"
-  subnets                    = [module.vpc.private_subnets]
-  security_group_list        = [module.security_groups.elastic_cache_memcache_security_group_id]
-  evictions_threshold        = 10
-  curr_connections_threshold = 500
-  internal_record_name       = "memcachedconfig"
-  create_route53_record      = true
-  internal_zone_id           = module.internal_zone.internal_hosted_zone_id
-  internal_zone_name         = module.internal_zone.internal_hosted_name
+  create_internal_zone_record = true
+  curr_connections_threshold  = 500
+  elasticache_engine_type     = "memcached14"
+  evictions_threshold         = 10
+  instance_class              = "cache.m4.large"
+  internal_record_name        = "memcachedconfig"
+  internal_zone_id            = module.internal_zone.internal_hosted_zone_id
+  internal_zone_name          = module.internal_zone.internal_hosted_name
+  name                        = "memc-${random_string.r_string.result}"
+  security_groups             = [module.security_groups.elastic_cache_memcache_security_group_id]
+  subnets                     = module.vpc.private_subnets
 
-  additional_tags = {
+  tags = {
     MyTag1 = "MyValue1"
     MyTag2 = "MyValue2"
     MyTag3 = "MyValue3"
@@ -70,18 +73,18 @@ module "elasticache_memcached" {
 module "elasticache_redis_multi_shard" {
   source = "../../module"
 
-  cluster_name            = "redms-${random_string.r_string.result}"
-  elasticache_engine_type = "redis50"
-  instance_class          = "cache.m4.large"
-  redis_multi_shard       = true
-  subnets                 = [module.vpc.private_subnets]
-  security_group_list     = [module.security_groups.elastic_cache_redis_security_group_id]
-  internal_record_name    = "multishardconfig"
-  create_route53_record   = true
-  internal_zone_id        = module.internal_zone.internal_hosted_zone_id
-  internal_zone_name      = module.internal_zone.internal_hosted_name
+  create_internal_zone_record = true
+  elasticache_engine_type     = "redis506"
+  instance_class              = "cache.m4.large"
+  internal_record_name        = "multishardconfig"
+  internal_zone_id            = module.internal_zone.internal_hosted_zone_id
+  internal_zone_name          = module.internal_zone.internal_hosted_name
+  name                        = "redms-${random_string.r_string.result}"
+  redis_multi_shard           = true
+  security_groups             = [module.security_groups.elastic_cache_redis_security_group_id]
+  subnets                     = module.vpc.private_subnets
 
-  additional_tags = {
+  tags = {
     MyTag1 = "MyValue1"
     MyTag2 = "MyValue2"
     MyTag3 = "MyValue3"
@@ -91,18 +94,18 @@ module "elasticache_redis_multi_shard" {
 module "elasticache_redis_1" {
   source = "../../module"
 
-  cluster_name            = "red-${random_string.r_string.result}-1"
-  elasticache_engine_type = "redis50"
-  instance_class          = "cache.t2.medium"
-  redis_multi_shard       = false
-  subnets                 = [module.vpc.private_subnets]
-  security_group_list     = [module.security_groups.elastic_cache_redis_security_group_id]
-  internal_record_name    = "redisconfigone"
-  create_route53_record   = true
-  internal_zone_id        = module.internal_zone.internal_hosted_zone_id
-  internal_zone_name      = module.internal_zone.internal_hosted_name
+  create_internal_zone_record = true
+  elasticache_engine_type     = "redis506"
+  instance_class              = "cache.t2.medium"
+  internal_record_name        = "redisconfigone"
+  internal_zone_id            = module.internal_zone.internal_hosted_zone_id
+  internal_zone_name          = module.internal_zone.internal_hosted_name
+  name                        = "red-${random_string.r_string.result}-1"
+  redis_multi_shard           = false
+  security_groups             = [module.security_groups.elastic_cache_redis_security_group_id]
+  subnets                     = module.vpc.private_subnets
 
-  additional_tags = {
+  tags = {
     MyTag1 = "MyValue1"
     MyTag2 = "MyValue2"
     MyTag3 = "MyValue3"
@@ -112,24 +115,22 @@ module "elasticache_redis_1" {
 module "elasticache_redis_2" {
   source = "../../module"
 
-  cluster_name               = "red-${random_string.r_string.result}-2"
-  elasticache_engine_type    = "redis50"
-  instance_class             = "cache.m4.large"
-  redis_multi_shard          = false
-  subnets                    = [module.vpc.private_subnets]
-  security_group_list        = [module.security_groups.elastic_cache_redis_security_group_id]
-  internal_record_name       = "redisconfigtwo"
-  create_route53_record      = true
-  internal_zone_id           = module.internal_zone.internal_hosted_zone_id
-  internal_zone_name         = module.internal_zone.internal_hosted_name
-  evictions_threshold        = 10
-  curr_connections_threshold = 500
+  create_internal_zone_record = true
+  curr_connections_threshold  = 500
+  elasticache_engine_type     = "redis506"
+  evictions_threshold         = 10
+  failover_enabled            = false
+  instance_class              = "cache.m4.large"
+  internal_record_name        = "redisconfigtwo"
+  internal_zone_id            = module.internal_zone.internal_hosted_zone_id
+  internal_zone_name          = module.internal_zone.internal_hosted_name
+  name                        = "red-${random_string.r_string.result}-2"
+  number_of_nodes             = 1
+  redis_multi_shard           = false
+  security_groups             = [module.security_groups.elastic_cache_redis_security_group_id]
+  subnets                     = module.vpc.private_subnets
 
-  # Test single-shard, single-node, no failover
-  number_of_nodes  = 1
-  failover_enabled = false
-
-  additional_tags = {
+  tags = {
     MyTag1 = "MyValue1"
     MyTag2 = "MyValue2"
     MyTag3 = "MyValue3"
@@ -139,32 +140,32 @@ module "elasticache_redis_2" {
 resource "random_string" "string_19" {
   length  = 19
   lower   = true
-  upper   = false
   number  = false
   special = false
+  upper   = false
 }
 
 module "elasticache_redis_constructed_cluster_name_20_chars" {
   source = "../../module"
 
-  cluster_name            = "${random_string.string_19.result}a"
-  cluster_name_version    = "${random_string.string_19.result}a"
-  elasticache_engine_type = "redis50"
+  elasticache_engine_type = "redis506"
   instance_class          = "cache.t2.medium"
+  name                    = "${random_string.string_19.result}a"
+  name_version            = "${random_string.string_19.result}a"
   redis_multi_shard       = false
-  subnets                 = [module.vpc.private_subnets]
-  security_group_list     = [module.security_groups.elastic_cache_redis_security_group_id]
+  security_groups         = [module.security_groups.elastic_cache_redis_security_group_id]
+  subnets                 = module.vpc.private_subnets
 }
 
 module "elasticache_redis_constructed_cluster_name_19_chars" {
   source = "../../module"
 
-  cluster_name            = random_string.string_19.result
-  cluster_name_version    = random_string.string_19.result
-  elasticache_engine_type = "redis50"
+  elasticache_engine_type = "redis506"
   instance_class          = "cache.t2.medium"
+  name                    = random_string.string_19.result
+  name_version            = random_string.string_19.result
   redis_multi_shard       = false
-  subnets                 = [module.vpc.private_subnets]
-  security_group_list     = [module.security_groups.elastic_cache_redis_security_group_id]
+  security_groups         = [module.security_groups.elastic_cache_redis_security_group_id]
+  subnets                 = module.vpc.private_subnets
 }
 
